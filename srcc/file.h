bafcc9feae9edea8c0b1a10e5c7bb13ae7a39036
kwphil <121273949+kwphil@users.noreply.github.com>
2023-10-05 12:32:45 -0500

    Create file.h

---
+++ srcc/file.h
@@ -0,0 +1,9 @@
+//Anything to do with reading/writing files will go in here
+#ifndef CFILE_H
+#define CFILE_H
+
+#include "../../srcp/types/type.h"
+
+string_t readFile(string_t filename);
+
+#endif
