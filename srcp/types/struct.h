9f159756773a9e6092fe499ff1e27a251221764c
kwphil <121273949+kwphil@users.noreply.github.com>
2023-10-10 08:32:55 -0500

    Update struct.h

--- srcp/types/struct.h
+++ srcp/types/struct.h
@@ -5,16 +5,21 @@
 
 //Specifically designed to have datatypes of type struct 
 
+typedef struct __loc_t {
+  int line;
+  int word;
+} loc_t;
+
 typedef struct __token_t {
   string_t type;
   int      value;
   loc_t    loc;
 } token_t;
 
-typedef struct __loc_t {
-  int line;
-  int word;
-} loc_t;
+typedef struct __asm_t {
+  char *ins;
+  loc_t loc;
+} asm_t;
 
 typedef struct __file_t {
   char *name;
@@ -22,10 +27,5 @@ typedef struct __file_t {
   token_t *tokenList;
   asm_t *asmList;
 } file_t;
-
-typedef struct __asm_t {
-  char *ins;
-  loc_t loc;
-} asm_t;
 
 #endif
