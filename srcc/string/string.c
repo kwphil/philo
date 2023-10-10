5659116a499e99de75f4e6ab3af4025032a00760
kwphil <121273949+kwphil@users.noreply.github.com>
2023-10-10 08:25:10 -0500

    Update string.c

--- srcc/string/string.c
+++ srcc/string/string.c
@@ -1,9 +1,19 @@
 #include ../../srcp/types/types.h
+#include <stdbool.h>
 #include <string.h>
+#include <ctype.h>
+#include <stdlib.h>
 
 string_t appendStr(string_t str1, string_t str2) {
   char ret[strlen(str1) + strlen(str2)];
 
   strcpy(ret, str1);
   strcat(ret, str2);
+}
+
+bool isNum(string_t str) {
+  while(str[i] != NULL)
+    if(!isdigit(str[i])) return false;
+
+  return true;
 }
