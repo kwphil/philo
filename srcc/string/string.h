3505311ac9198fef35f0b6958ec34b9ed5801a56
kwphil <121273949+kwphil@users.noreply.github.com>
2023-10-10 08:26:44 -0500

    Update string.h

--- srcc/string/string.h
+++ srcc/string/string.h
@@ -1,10 +1,15 @@
 #ifndef CSTRING_H
 #define CSTRING_H
 
-#include ../../srcp/types/types.h
+#include "../../srcp/types/types.h"
+#include <stdbool.h>
 //used for some extra string functions
 
 //outputs the concatenation of two strings without changing either
 string_t appendStr(string_t str1, string_t str2);
+
+//Checks if an entire string is a number
+//Returns true if the entire string is a number
+bool isNum(string_t str);
 
 #endif
