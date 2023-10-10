6f61c9504e3a978f80ab0b86493ad52bc7a7b7db
kwphil <121273949+kwphil@users.noreply.github.com>
2023-10-10 13:02:49 -0500

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 #include "srcp/token/token.h"
 #include "srcc/file/file.h"
 #include "srcp/types/struct.h"
@@ -9,19 +11,23 @@ int main(int argc, char *argv[]) {
 
     if(argc != 2) {
         bError = true;
-        sError = "Incorrect Usage! Correct usage is ./philo <file>";
+        char _sError = "Incorrect Usage! Correct usage is ./philo <file>";
+        realloc(sError, sizeof(_sError));
+        strcpy(sError, _sError);
         setErr();
         return 1;
     }
-    return run(argv[1]);
+    return run(argv);
 }
 
-int run(char *filename) {
-    string_t mainFileOutput = readFile(filename);
+int run(char *argv[]) {
+    string_t mainFileOutput = readFile(argv[1]);
     token_t *tokenList      = tokenize(mainFileOutput);
     asm_t    asmList        = convertAsm(tokenList);
 
     if(bError) return errorRet();
 
-    createBinFile(asmList);
+    build(asmList);
+
+    printf("%s was compiled successfully! You can run it at build.bin!", argv[1]);
 }
