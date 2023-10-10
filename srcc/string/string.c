551e27ee81a45aa98913eadbc4dab5f0573edb03
kwphil <121273949+kwphil@users.noreply.github.com>
2023-10-10 11:50:24 -0500

    Update string.c

--- srcc/string/string.c
+++ srcc/string/string.c
@@ -1,8 +1,23 @@
-#include ../../srcp/types/types.h
+#include "../../srcp/types/types.h"
 #include <stdbool.h>
 #include <string.h>
 #include <ctype.h>
 #include <stdlib.h>
+
+string_t substr(string_t str, int start, int end) {
+  if(start < 0) start -= strlen(str) - 1;
+  if(end <= 0) end -= strlen(str) - 1;
+
+  char ret[end - start + 1];
+
+  for(int i = start, j = 0; i < end; i++) {
+    ret[j] = str[i];
+
+    j++;
+  }
+  
+  return j;
+}
 
 string_t appendStr(string_t str1, string_t str2) {
   char ret[strlen(str1) + strlen(str2)];
