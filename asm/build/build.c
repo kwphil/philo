1ec4ee751cdc5b0c77605d93dd7b0af367f9b2d0
kwphil <121273949+kwphil@users.noreply.github.com>
2023-10-10 12:17:22 -0500

    Update build.c

--- asm/build/build.c
+++ asm/build/build.c
@@ -3,29 +3,36 @@
 #include <string.h>
 #include <unistd.h>
 #include "../../srcc/file/file.h"
+#include "../../srcc/string/string.h"
 #include "../../srcp/types/struct.h"
 #include "../../srcp/types/types.h"
 
 int build(const asm_t *asmList, const char argv[]) {
     char fileContents[] = "section .text 
                              global _start 
-                             _start:";
-    int i = 0, j = 0, count = 0, sect = 0;
+                             _start: ";
+    int i = 0, j = 0, sect = 0;
 
     while(true) {
         while(asmList[i++].ins[0] != NULL) {
-            
+            if(asmList[i].section != sect || asmList[i].loc != j) 
+                continue;
+
+            realloc(fileContents, sizeof(fileContents) + strlen(asmList[i].ins) + sizeof(char)); //The original file length, the new instruction, and a space to separate
+
+            strcat(fileContents, appendStr(asmList[i].ins, " "));
+            free(currIns);
+            j++;
         }
-        count++;
-        if(count >= 5 && sect <= 3) {
+
+        if(sect <= 3) {
             j = 0;
-            realloc(fileContents, sizeof(fileContents[i]) + (sizeof(char) * 13)) // section .(sect)
+            realloc(fileContents, sizeof(fileContents) + (sizeof(char) * 13)) // section .(sect)
             strcat(fileContents, " section .")
             if(sect == 1) strcat(fileContents, "data");
             else(strcat(fileContents, "bss"));
             sect++;
-        }
-        if(sect > 3) break;
+        } else break;
     }
 
     writeFile("temp.s", fileContents);
