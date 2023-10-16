08172703f02cf6b7943fca1300371fde6288dc94
kwphil <121273949+kwphil@users.noreply.github.com>
2023-10-16 13:29:39 -0500

    Update syntax.c

--- srcp/syntax/syntax.c
+++ srcp/syntax/syntax.c
@@ -1,5 +1,52 @@
 const char *keywordSynt[] = {"^ 'int'", //return
                              "^('bool') 'exprList'", //while 
                              "^('bool') 'exprList'", //if
-                             "^('expr1';'expr2';'expr3') 'exprList'" //for
+                             "^('expr';'expr';'expr') 'exprList'" //for
                             };
+const char *operatorSynt[] = {"^ 'macro'",
+                              "^ 'asmLine'",
+                              "^'expr')",
+                              "",
+                              "^'expr']",
+                              "'structObj'^'var'",
+                              "'macroList'^'macro'",
+                              "'int'^",
+                              "'int'^",
+                              "'expr +COLAVAILIABLE'^'colExpr'"
+                              "'int'^'int' | 'char`'^'char`'",
+                              "'int'^'int' | 'char`'^'int'",
+                              "^'bool",
+                              "^'int'",
+                              "^'var'",
+                              "^'var'",
+                              "^('ANY')",
+                              "^('macro', 'int') | ^('macro')",
+                              "'int'^'int' | 'char`'^'int'",
+                              "'int'^'int'",
+                              "'int'^'int'",
+                              "'int'^'int'",
+                              "'int'^'int'",
+                              "'int'^'int'",
+                              "'int'^'int'",//<=
+                              "'int'^'int'",
+                              "'int'^'int'",
+                              "'expr -void'^'expr -void'",
+                              "'expr -void'^'expr -void'",
+                              "'int'^'int'",
+                              "'int'^'int'",
+                              "'int'^'int'",
+                              "'bool'^'value':'value'",
+                              "",
+                              "'var'^'value'",
+                              "'var'^'value'",
+                              "'var'^'value'",
+                              "'var'^'value'", //*=
+                              "'var'^'int'",
+                              "'var'^'int'",
+                              "'var'^'int'",
+                              "'var'^'int'",
+                              "'var'^'int'",
+                              "'var'^'int'",
+                              "'var'^'int'",
+                              "'value'^'value'"
+                             }
