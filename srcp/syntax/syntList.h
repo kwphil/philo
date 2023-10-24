b83914610bbf787e64212831a055f7b125f0a9e5
kwphil <121273949+kwphil@users.noreply.github.com>
2023-10-24 11:00:15 -0500

    Update syntList.h

--- srcp/syntax/syntList.h
+++ srcp/syntax/syntList.h
@@ -1,6 +1,10 @@
 #ifndef PSYNTLIST_H
 #define PSYNTLIST_H
 
+//Is not really needed in most of the program except for in syntax.c, it is used
+//If you are wanting to add some new syntax, you can access it in syntList.c
+void insertSyntList(struct syntStruct_t *syntaxList, int syntLoc);
+
 struct syntStruct_t {
     bool    dirDefined;
     bool    required;
