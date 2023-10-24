d541b4e9257178338259063f00237152cd909c06
kwphil <121273949+kwphil@users.noreply.github.com>
2023-10-24 10:05:34 -0500

    Update syntax.c

--- srcp/syntax/syntax.c
+++ srcp/syntax/syntax.c
@@ -113,7 +113,7 @@ void insertSyntList(struct syntStruct_s *syntaxList, int syntLoc) {
     if(syntList[currToken.type - 1][syntLoc][0] == '\0') return true;
 
     bool inSect = false, incl = false, excl = false;
-    char currText = "";
+    char *currText[2] = {"", ""};
 
     for(int i = 0, j = 0; i < strlen(syntList[currToken.type - 1][syntLoc]); i++) {
         if(inSect) {
@@ -136,18 +136,40 @@ void insertSyntList(struct syntStruct_s *syntaxList, int syntLoc) {
 
             if(syntList[currToken.type - 1] == '+') {
                 incl = true;
+                realloc(currText[1], sizeof(char));
             }
 
             if(syntList[currToken.type - 1] == ' ') {
-                if(!incl && !excl) break;
+                if(incl) {
+                    int l = 0;
+                    while(syntaxList[j].include[l] != NULL);
+                    realloc(sizeof(syntaxList[j].include, syntaxList[j].include * sizeof(*syntaxList[j].include)));
+                    strcpy(syntaxList[j].include[l], currText[1]);
 
+                    incl = false;
+                    realloc(currText[1], 0);
+
+                    continue;
+                } else if(excl) {
+                    int l = 0;
+                    while(syntaxList[j].exclude[l] != NULL);
+                    realloc(syntaxList[j].exclude, sizeof(syntaxList[j].exclude) * sizeof(*syntaxList[j].exclude));
+                    strcpy(syntaxList[j].exclude[l], currText[1]);
+
+                    excl = false;
+                    realloc(currText[1], 0);
+
+                    continue;
+                }
+            } else if(incl || excl) {
+                realloc(currText[1], sizeof(currText[1]) + sizeof(char));
+                currText[1][strlen(currText[1])] = syntList[currToken.type - 1];
+            } else {
+                realloc(currText, sizeof(currText) + sizeof(char));
+                currText[strlen(currText) - 1] = syntList[currToken.type - 1][syntLoc][i];
             }
 
-            continue;
-
-            NONE:
-            realloc(currText, sizeof(currText) + sizeof(char));
-            currText[strlen(currText) - 1] = syntList[currToken.type - 1][syntLoc][i];
+            continue;            
         }
 
         if(syntList[currToken.type - 1][syntLoc][i] == '\'') {
