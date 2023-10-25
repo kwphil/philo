9ef350ec033a9c84274998f92242888a7b1f800f
kwphil <121273949+kwphil@users.noreply.github.com>
2023-10-25 10:26:35 -0500

    Update syntax.h

---
+++ srcp/syntax/syntax.h
@@ -0,0 +1,9 @@
+#ifndef PSYNTAX_H
+#define PSYNTAX_H
+
+/*  Used to check if all syntax is correct. will return true if the syntax does not have an error
+ *  Else return false;
+ */
+bool checkSyntax();
+
+#endif
