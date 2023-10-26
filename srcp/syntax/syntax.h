2a445c33a31ff9d6a8e95aff9657898c7db8e228
kwphil <121273949+kwphil@users.noreply.github.com>
2023-10-26 09:55:03 -0500

    Update syntax.h

--- srcp/syntax/syntax.h
+++ srcp/syntax/syntax.h
@@ -1,6 +1,9 @@
 #ifndef PSYNTAX_H
 #define PSYNTAX_H
 
+//List of syntax-use cases
+const char *syntList[][];
+
 /*  Used to check if all syntax is correct. will return true if the syntax does not have an error
  *  Else return false;
  */
