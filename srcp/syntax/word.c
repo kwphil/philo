10c56c6b04f5015fc8a28d83d98aef54e0576eea
kwphil <121273949+kwphil@users.noreply.github.com>
2023-10-26 09:51:25 -0500

    Update word.c

--- srcp/syntax/word.c
+++ srcp/syntax/word.c
@@ -1,3 +1,3 @@
 bool checkCurrSyntax(const token_t currToken, const uint8_t currTokenLoc, const syntStruct_t *syntaxList, const int currCheck) {
-       
+    if(syntaxList[currCheck].dirDefined) return matchstr()
 }
