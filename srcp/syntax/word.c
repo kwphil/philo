4ea22770d48e5401897ce608162028902ab96a2f
kwphil <121273949+kwphil@users.noreply.github.com>
2023-10-26 10:37:38 -0500

    Update word.c

--- srcp/syntax/word.c
+++ srcp/syntax/word.c
@@ -1,5 +1,9 @@
 #include "syntax.h"
 
 bool checkCurrSyntax(const token_t currToken, const uint8_t currTokenLoc, const syntStruct_t *syntaxList, const int currCheck) {
-    if(syntaxList[currCheck].dirDefined) return matchstr(syntaxList[currCheck].value, )
+    if(syntaxList[currCheck].dirDefined) 
+        return matchstr(syntaxList[currCheck].value, 
+                        syntList[tokenTypeToNum(syntaxList[currCheck].type)]);
+
+    
 }
