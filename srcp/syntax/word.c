e34eb535d6809f723a8b620dc0eda084d5b1395a
kwphil <121273949+kwphil@users.noreply.github.com>
2023-10-27 10:38:52 -0500

    Update word.c

--- srcp/syntax/word.c
+++ srcp/syntax/word.c
@@ -1,4 +1,8 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
 #include "syntax.h"
+#include "../../srcc/string/string.h"
 
 bool checkCurrSyntax(const token_t currToken, const uint8_t currTokenLoc, 
                      const struct syntStruct_t *syntaxList, 
@@ -7,15 +11,15 @@ bool checkCurrSyntax(const token_t currToken, const uint8_t currTokenLoc,
     if(syntaxList[currCheck].multiple) {
         register int i = 0;
         
-        while(checkCurrSyntax0(getNextToken(tokenNum), 
+        while(checkCurrSyntax0(currFile.tokenList[tokenNum], 
                                currTokenLoc + (++i), 
                                syntaxList, 
                                currCheck + i, 
-                               tokenNum + i))
-        {
-            
-        }
+                               tokenNum + i)) if(bError) return false;
         
+        currCheck + i;
+        tokenNum + i;
+        return true;
     }
     
     return checkCurrSyntax0(currToken, currTokenLoc syntaxList, currCheck, tokenNum);
@@ -29,4 +33,6 @@ bool checkCurrSyntax0(const token_t currToken, const uint8_t currTokenLoc,
     if(syntaxList[currCheck].dirDefined) 
         return matchstr(syntaxList[currCheck][].value, 
                         currFile.tokenList[tokenNum].value);
+
+
 }
