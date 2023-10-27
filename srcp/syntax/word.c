27cd9a6168a98ac8994d55f25b82e5df8b839d3c
kwphil <121273949+kwphil@users.noreply.github.com>
2023-10-27 09:26:23 -0500

    Update word.c

--- srcp/syntax/word.c
+++ srcp/syntax/word.c
@@ -1,9 +1,17 @@
 #include "syntax.h"
 
 bool checkCurrSyntax(const token_t currToken, const uint8_t currTokenLoc, const syntStruct_t *syntaxList, const int currCheck) {
+    if(syntaxList[currCheck].multiple) {
+        int i = 0;
+        
+        while(syntList[tokenTypeToNum(syntaxList[currCheck].type)])
+    }
+    
+    return checkCurrSyntax0(currToken, currTokenLoc syntaxList, currCheck)
+}
+
+bool checkCurrSyntax0(const token_t currToken, const uint8_t currTokenLoc, const syntStruct_t *syntaxList, const int currCheck) {
     if(syntaxList[currCheck].dirDefined) 
         return matchstr(syntaxList[currCheck].value, 
-                        syntList[tokenTypeToNum(syntaxList[currCheck].type)]);
-
-    
+                        syntList[tokenTypeToNum(syntaxList[currCheck].type)]); //Needs work here
 }
