6b59fcd107c2291b57eca61203af7c9a1fd85c41
kwphil <121273949+kwphil@users.noreply.github.com>
2023-10-31 13:53:35 -0500

    Update word.c

--- srcp/syntax/word.c
+++ srcp/syntax/word.c
@@ -32,11 +32,10 @@ bool checkCurrSyntax0(const token_t currToken, const uint8_t currTokenLoc,
     if(syntaxList[currCheck].dirDefined) { 
         if(syntaxList[currCheck].value == ';' && currFile.tokenList[tokenNum].value != ';' && SEMI_WARN_FLAG && !SEMI_ERR_FLAG) {
             bWarning = true;
-            _sWarning = appendf(
-                        "Warning! Missing semicolon at %s::%d:%d\n%s", 
-                        currFile.name, currFile.tokenList[tokenNum].loc.line, 
-                        currFile.tokenList[tokenNum].loc.word,
-                        "Note: Semicolons are not required but are recommended\n");
+            _sWarning = appendf("Warning! Missing semicolon at %s::%d:%d\n%s", 
+                                currFile.name, currFile.tokenList[tokenNum].loc.line, 
+                                currFile.tokenList[tokenNum].loc.word,
+                                "Note: Semicolons are not required but are recommended\n");
             realloc(sWarning, sizeof(_sWarning));
             strcpy(sWarning, _sWarning);
             return true;
