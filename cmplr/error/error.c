6ab9a0636f3b73a29b77a2b4dfe29ba40e41648c
kwphil <121273949+kwphil@users.noreply.github.com>
2023-11-01 08:19:03 -0500

    Update error.c

--- cmplr/error/error.c
+++ cmplr/error/error.c
@@ -1,6 +1,8 @@
 #include "error.h"
 #include "../../srcc/file/read.h"
 
+extern bool bError;
+
 void setErr() {
     if(!bError) return;
     printf("Error at %d:%d! %s", currLoc.line, currLoc.word, sError);
