b89829b2b4fc1789ffee45f5b8f610be3968e2b2
kwphil <121273949+kwphil@users.noreply.github.com>
2023-11-03 11:23:37 -0500

    Update func.h

--- srcp/func/func.h
+++ srcp/func/func.h
@@ -1,6 +1,15 @@
+#ifndef PFUNC_H
+#define PFUNC_H
+
+#include "../param/param.h"
+
+func_t parseFunc(loc_t loc);
+
 typedef struct __func_t {
     int fnType;
     int retType;
     char *name;
     param_t *param;
 } func_t;
+
+#endif
