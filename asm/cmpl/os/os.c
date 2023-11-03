faeac811e50e5b3d9cca94faba6eede6c51ac55d
kwphil <121273949+kwphil@users.noreply.github.com>
2023-11-03 10:26:16 -0500

    Update os.c

---
+++ asm/cmpl/os/os.c
@@ -0,0 +1,9 @@
+#include <stdint.h>
+
+void OSCompile() {
+    for(uint16_t fileInc = 0; fileList[fileInc].name != NULL; fileInc++) {
+        for(uint32_t tokenInc = 0; fileList[fileInc].tokenList[tokenInc].value != NULL; tokenInc++) {
+            
+        }
+    }
+}
