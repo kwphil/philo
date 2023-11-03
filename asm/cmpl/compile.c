c0cc13932b96ad1ed316e65732df4b4dd6b150da
kwphil <121273949+kwphil@users.noreply.github.com>
2023-11-03 09:58:55 -0500

    Update compile.c

--- asm/cmpl/compile.c
+++ asm/cmpl/compile.c
@@ -1,8 +1,22 @@
+#include <stdlib.h>
+#include <stdbool.h>
+#include <stdio.h>
 #include "../../srcp/macro/read.h"
 #include "../../srcp/types/struct.h"
 
-asmCompile() {
+bool asmCompile() {
+    printf("No syntax errors caught! Starting the compile process!");
+
     char *prgmFor = findMacro(prgmFor, NULL);
+    int i = 0;
+
+    while(CMPL_LIST.data != NULL) {
+        if(strcmp(prgmFor, CMPL_LIST.data)) {
+            CMPL_LIST.func;
+
+            if(bError) return false;
+        }
+    }
 
     
 }
