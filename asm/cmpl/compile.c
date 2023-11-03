a1653f50dde672a83bd08946d3b2e3d3f8aeb92d
kwphil <121273949+kwphil@users.noreply.github.com>
2023-11-03 10:05:52 -0500

    Update compile.c

--- asm/cmpl/compile.c
+++ asm/cmpl/compile.c
@@ -4,24 +4,25 @@
 #include "../../srcp/macro/read.h"
 #include "../../srcp/types/struct.h"
 
-static funcOrg_t *CMPL_LIST 
+static funcOrg_t CMPL_LIST[1]; // Define an array with one element
 
 bool asmCompile() {
     printf("No syntax errors caught! Starting the compile process!");
 
     char *prgmFor = findMacro(prgmFor, NULL);
-    int i = 0;
-
-    while(CMPL_LIST.data != NULL) {
-        if(strcmp(prgmFor, CMPL_LIST.data)) {
-            CMPL_LIST.func;
-
-            if(bError) return false;
+    
+    if (strcmp(prgmFor, CMPL_LIST[0].data) == 0) {
+        // Call the function
+        CMPL_LIST[0].func();
+        if (bError) {
+            return false;
         }
     }
+    
+    return true;
 }
 
 void setCMPL_LIST() {
     CMPL_LIST[0].data = "OS";
-    CMPL_LIST[0].func = *OSCompile();
+    CMPL_LIST[0].func = OSCompile; // Assuming OSCompile is a function pointer
 }
