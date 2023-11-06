10ac8855424de9bb13722fcf76706eaca77bd3ed
kwphil <121273949+kwphil@users.noreply.github.com>
2023-11-06 10:33:10 -0600

    Update func.c

--- srcp/func/func.c
+++ srcp/func/func.c
@@ -6,7 +6,7 @@
 #include "../param/param.h"
 
 func_t parseFunc(const int file, const int tokenNum) {
-    if(!matchstr(fileList[file].tokenList[tokenNum].value, "fn")) {
+    if (strcmp(fileList[file].tokenList[tokenNum].value, "fn") != 0) {
         bError = true;
         const char _sError[] = appendStr("Compiler Error! Expected 'fn' but received: ", fileList[file].tokenList[tokenNum].value);
         realloc(sError, sizeof(_sError));
@@ -15,54 +15,56 @@ func_t parseFunc(const int file, const int tokenNum) {
     }
 
     func_t ret;
-    register int tokenNumOff = 1;
+    int tokenNumOff = 1;
 
-    if(!fileList[file].tokenList[tokenNum + tokenNumOff].value[0] == ':') {
-        if(NO_COL_WARN_FLAG) {
+    if (fileList[file].tokenList[tokenNum + tokenNumOff].value[0] == ':') {
+        if (!NO_COL_WARN_FLAG) {
             bWarning = true;
-            const char _sWarning = "Warning! No colon operator present!";
+            const char _sWarning[] = "Warning! No colon operator present!";
             realloc(sWarning, sizeof(_sWarning));
-            strcpy(sWarning, sizeof(_sWarning));
+            strcpy(sWarning, _sWarning);
         }
-
         ret.fnType = 0;
         tokenNumOff++;
     }
 
-    if(!fileList[file].tokenList[tokenNum + tokenNumOff].type == 4) {
+    if (fileList[file].tokenList[tokenNum + tokenNumOff].type != TOKEN_TYPE) {
         bError = true;
-        const char _sError[] = appendStr("Compiler Error! Expected <dataType> but received: ", fileList[file].tokenList[tokenNumOff + 1].value);
+        const char _sError[] = appendStr("Compiler Error! Expected <dataType> but received: ", fileList[file].tokenList[tokenNumOff].value);
         realloc(sError, sizeof(_sError));
         strcpy(sError, _sError);
         return defaultFunc();
     }
 
-    ret.retType = matchRetType(fileList[file].tokenList[tokenNum + tokenNumOff].type);
-    ret.name = (char *)malloc(sizeof(fileList[file].tokenList[tokenNum + ++tokenNumOff].value));
-    strcpy(ret.name, fileList[file].tokenList[tokenNum + tokenNumOff].value);
+    ret.retType = matchRetType(fileList[file].tokenList[tokenNum + tokenNumOff].value);
+    ret.name = strdup(fileList[file].tokenList[tokenNum + tokenNumOff + 1].value);
 
     int sectNum = 0;
 
-    ret.param = setParamList(file, tokenNum + tokenNumOff);
-    while(sectNum == 0 && fileList[file].tokenList[tokenNum + ++tokenNumOff].value != ')') {
-        if(fileList[file].tokenList[tokenNum + tokenNumOff].value == '(') {
+    ret.param = setParamList(file, tokenNum + tokenNumOff + 3);
+    while (sectNum == 0 && strcmp(fileList[file].tokenList[tokenNum + tokenNumOff].value, ")") != 0) {
+        if (strcmp(fileList[file].tokenList[tokenNum + tokenNumOff].value, "(") == 0) {
             sectNum++;
-            continue;
         }
-        if(fileList[file].tokenList[tokenNum + tokenNumOff].value == ')') sectNum--;
+        if (strcmp(fileList[file].tokenList[tokenNum + tokenNumOff].value, ")") == 0) {
+            sectNum--;
+        }
+        tokenNumOff++;
     }
 
-    ret.start = tokenNum + ++tokenNumOff;
+    ret.start = tokenNum + tokenNumOff + 1;
     sectNum = 0;
-    while(sectNum == 0 && fileList[file].tokenList[tokenNum + ++tokenNumOff].value != '}') {
-        if(fileList[file].tokenList[tokenNum + tokenNumOff].value == '{') {
+    while (sectNum == 0 && strcmp(fileList[file].tokenList[tokenNum + tokenNumOff].value, "}") != 0) {
+        if (strcmp(fileList[file].tokenList[tokenNum + tokenNumOff].value, "{") == 0) {
             sectNum++;
-            continue;
         }
-        if(fileLIst[file].tokenList[tokenNum + tokenNumOff].value == '}') sectNum--;
+        if (strcmp(fileList[file].tokenList[tokenNum + tokenNumOff].value, "}") == 0) {
+            sectNum--;
+        }
+        tokenNumOff++;
     }
 
-    ret.end = tokenNum + ++tokenNumOff;
+    ret.end = tokenNum + tokenNumOff + 1;
 
     return ret;
 }
@@ -72,9 +74,7 @@ func_t defaultFunc() {
 
     ret.fnType = 0;
     ret.retType = 0;
-    ret.name = (char *)malloc(sizeof(char));
     ret.name = NULL;
-    ret.param = (param_t)malloc(sizeof(param_t));
     ret.param = defaultParam();
 
     return ret;
