b343da799911f0c90b2578e7bfd04180f22aca3b
kwphil <121273949+kwphil@users.noreply.github.com>
2023-11-06 10:44:34 -0600

    Update os.c

--- asm/cmpl/os/os.c
+++ asm/cmpl/os/os.c
@@ -2,17 +2,15 @@
 #include <stdint.h>
 #include <stdlib.h>
 
-func_t *funcList;
-
 void OSCompile() {
-    funcList = (func_t)malloc(sizeof(func_t));
+    func_t funcList = (func_t)malloc(sizeof(func_t));
     size_t funcSize = 0;
 
     for(uint16_t fileInc = 0; fileList[fileInc].name != NULL; fileInc++) {
         for(uint32_t tokenInc = 0; fileList[fileInc].tokenList[tokenInc].value != NULL; tokenInc++) {
             if(strcmp(fileList[fileInc].tokenList[tokenInc].value, "fn")) {
-                realloc(funcList, funcSize += sizeof(func_t));
-                
+                realloc(funcList, ++funcSize * sizeof(func_t));
+                funcList[funcSize - 1] = 
             }
         }
     }
