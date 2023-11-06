55c9a8c8a250e3a367cc2ef4b2d645f5b321e218
kwphil <121273949+kwphil@users.noreply.github.com>
2023-11-06 09:47:50 -0600

    Update func.c

--- srcp/func/func.c
+++ srcp/func/func.c
@@ -1,9 +1,10 @@
 #include <stdlib.h>
 #include <string.h>
 #include "func.h"
+#include "../../cmplr/flag/flag.h"
 #include "../param/param.h"
 
-func_t parseFunc(file_t file, int tokenNum) {
+func_t parseFunc(const int file, const int tokenNum) {
     if(!matchstr(fileList[file].tokenList[tokenNum].value, "fn")) {
         bError = true;
         const char _sError[] = appendStr("Compiler Error! Expected 'fn' but received: ", fileList[file].tokenList[tokenNum].value);
@@ -13,6 +14,28 @@ func_t parseFunc(file_t file, int tokenNum) {
     }
 
     func_t ret;
+    register int tokenNumOff = 1;
+
+    if(!fileList[file].tokenList[tokenNum + tokenNumOff].value[0] == ':') {
+        if(NO_COL_WARN_FLAG) {
+            bWarning = true;
+            const char _sWarning = "Warning! No colon operator present!";
+            realloc(sWarning, sizeof(_sWarning));
+            strcpy(sWarning, sizeof(_sWarning));
+        }
+
+        ret.fnType = 0;
+    }
+
+    if(!fileList[file].tokenList[tokenNum + tokenNumOff].type == 4) {
+        bError = true;
+        const char _sError[] = appendStr("Compiler Error! Expected ':' or <dataType> but received: ", fileList[file].tokenList[tokenNumOff + 1].value);
+        realloc(sError, sizeof(_sError));
+        strcpy(sError, _sError);
+        return defaultFunc();
+    }
+
+    ret.retType = matchRetType(fileList[file].tokenList[tokenNum + tokenNumOff].type);
 
     
 }
@@ -22,8 +45,8 @@ func_t defaultFunc() {
 
     fnType = 0;
     retType = 0;
-    str = (char *)malloc(sizeof(char));
-    str = NULL;
+    name = (char *)malloc(sizeof(char));
+    name = NULL;
     param = (param_t)malloc(sizeof(param_t));
     param = defaultParam();
 }
