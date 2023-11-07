6c3f68617ef8d81ea7588275502542255646a26b
kwphil <121273949+kwphil@users.noreply.github.com>
2023-11-07 10:09:04 -0600

    Update os.c

--- asm/cmpl/os/os.c
+++ asm/cmpl/os/os.c
@@ -9,66 +9,110 @@
 const asm_t startMsg[] = {asm_t(0, "push bp"), asm_t(0, "mov sp, bp")};
 
 void OSCompile() {
-    func_t *funcList = (func_t)malloc(sizeof(func_t));
+    // Allocate memory for the funcList based on an initial size.
+    size_t initialFuncListSize = 10;  // Adjust the size as needed.
+    func_t* funcList = (func_t*)malloc(initialFuncListSize * sizeof(func_t));
+    if (funcList == NULL) {
+        // Handle memory allocation error.
+        return;
+    }
     size_t funcSize = 0;
+
     bool mainSet = false;
     bool startSet = false;
     int startLoc;
 
-    for(uint16_t fileInc = 0; fileList[fileInc].name != NULL; fileInc++) {
-        for(uint32_t tokenInc = 0; fileList[fileInc].tokenList[tokenInc].value != NULL; tokenInc++) {
-            if(strcmp(fileList[fileInc].tokenList[tokenInc].value, "fn")) {
-                realloc(funcList, ++funcSize * sizeof(func_t));
-                funcList[funcSize - 1] = parseFunc(fileInc, tokenInc);
-                if(bError) return;
+    for (uint16_t fileInc = 0; fileList[fileInc].name != NULL; fileInc++) {
+        for (uint32_t tokenInc = 0; fileList[fileInc].tokenList[tokenInc].value != NULL; tokenInc++) {
+            if (!strcmp(fileList[fileInc].tokenList[tokenInc].value, "fn")) {
+                // Check if funcList needs to be expanded.
+                if (funcSize >= initialFuncListSize) {
+                    initialFuncListSize *= 2;
+                    func_t* newFuncList = (func_t*)realloc(funcList, initialFuncListSize * sizeof(func_t));
+                    if (newFuncList == NULL) {
+                        free(funcList);
+                        // Handle memory allocation error.
+                        return;
+                    }
+                    funcList = newFuncList;
+                }
+
+                funcList[funcSize] = parseFunc(fileInc, tokenInc);
+                if (bError) {
+                    free(funcList);  // Free the memory allocated.
+                    return;
+                }
+                funcSize++;
             }
         }
     }
 
-    char *definedNames[aLen(funcList)] = "";
+    char* definedNames[funcSize] = {0};  // Initialize an array to hold function names.
 
-    for(uint32_t i = 0; funcList[i].name != NULL; i++) {
-        if(strcmp(funcList[i].name, mainSet)) {
-            if(startSet) {
+    for (size_t i = 0; i < funcSize; i++) {
+        if (!strcmp(funcList[i].name, "main")) {
+            if (mainSet) {
                 bError = true;
-                const char _sError = "Definitions of both main() and start()!";
-                realloc(sError, _sError);
+                const char _sError[] = "Multiple definitions of main function!";
+                realloc(sError, sizeof(_sError));
                 appendStr(sError, _sError);
+                free(funcList);  // Free the memory allocated.
                 return;
             }
+            mainSet = true;
+        }
 
+        if (!strcmp(funcList[i].name, "start")) {
+            if (startSet) {
+                bError = true;
+                const char _sError[] = "Multiple definitions of start function!";
+                realloc(sError, sizeof(_sError));
+                appendStr(sError, _sError);
+                free(funcList);  // Free the memory allocated.
+                return;
+            }
+            startSet = true;
             startLoc = i;
         }
 
-        if(strcmp(funcList[i].name, startSet)) {
-            if(mainSet) {
+        for (size_t j = 0; j < funcSize; j++) {
+            if (i == j) continue;
+            if (!strcmp(funcList[i].name, funcList[j].name)) {
                 bError = true;
-                const char _sError = "Definitions of both main() and start()!";
-                realloc(sError, _sError);
+                const char _sError[] = appendStr("Multiple definitions of function: ", funcList[i].name, "!");
+                realloc(sError, sizeof(_sError));
                 appendStr(sError, _sError);
-                return;
-            }
-
-            startLoc = i;
-        }
-
-        for(uint32_t j = 0; funcList[j].name != NULL; j++) {
-            if(i == j) continue;
-            if(strcmp(funcList[i].name, funcList[j].name)) {
-                bError = true;
-                const char _sError = appendStr("Multiple definitions of function: "), funcList[i].name, "!";
-                realloc(sError, _sError);
-                appendStr(sError, _sError);
+                free(funcList);  // Free the memory allocated.
                 return;
             }
         }
     }
 
-    if((funcList[startLoc].retType != 1 && mainSet) || (funcList[startLoc].retType != 0 && startSet)) {
+    if (mainSet) {
+        if (funcList[startLoc].retType != 1) {
+            bError = true;
+            const char _sError[] = "start() is defined incorrectly!";
+            realloc(sError, sizeof(_sError));
+            appendStr(sError, _sError);
+            free(funcList);  // Free the memory allocated.
+            return;
+        }
+    } else if (startSet) {
+        if (funcList[startLoc].retType != 0) {
+            bError = true;
+            const char _sError[] = "main() is defined incorrectly!";
+            realloc(sError, sizeof(_sError));
+            appendStr(sError, _sError);
+            free(funcList);  // Free the memory allocated.
+            return;
+        }
+    } else {
         bError = true;
-        const char _sError = appendStr("start() or main() is defined incorrectly!\n received: ", printFunc(funcList[startLoc]), "\nNote: it should look like \nfn void start()\nfn int main(int argc, char *argv[])");
-        realloc(sError, _sError);
+        const char _sError[] = "No start() or main() function defined!";
+        realloc(sError, sizeof(_sError));
         appendStr(sError, _sError);
+        free(funcList);  // Free the memory allocated.
+        return;
     }
 
     //from here we can start putting the program together
