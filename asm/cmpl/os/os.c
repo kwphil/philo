bbe02749acc89e04c5c9fbc1145274ce24cc5129
kwphil <121273949+kwphil@users.noreply.github.com>
2023-11-15 16:51:35 -0600

    Update os.c

--- asm/cmpl/os/os.c
+++ asm/cmpl/os/os.c
@@ -30,26 +30,7 @@ void OSCompile() {
 
     for (uint16_t fileInc = 0; fileList[fileInc].name != NULL; fileInc++) {
         for (uint32_t tokenInc = 0; fileList[fileInc].tokenList[tokenInc].value != NULL; tokenInc++) {
-            if (!strcmp(fileList[fileInc].tokenList[tokenInc].value, "fn")) {
-                // Check if funcList needs to be expanded.
-                if (funcSize >= initialFuncListSize) {
-                    initialFuncListSize *= 2;
-                    func_t* newFuncList = (func_t*)realloc(funcList, initialFuncListSize * sizeof(func_t));
-                    if (newFuncList == NULL) {
-                        free(funcList);
-                        // Handle memory allocation error.
-                        return;
-                    }
-                    funcList = newFuncList;
-                }
-
-                funcList[funcSize] = parseFunc(fileInc, tokenInc);
-                if (bError) {
-                    free(funcList);  // Free the memory allocated.
-                    return;
-                }
-                funcSize++;
-            }
+            os_set_func();
         }
     }
 
@@ -130,4 +111,27 @@ void OSCompile() {
     cmpl.setCompileUntil("bits", 510);
     os.compile2ASM(0, i);
     os.addLines();
+}
+
+void os_set_func() {
+    if (!strcmp(fileList[fileInc].tokenList[tokenInc].value, "fn")) {
+        // Check if funcList needs to be expanded.
+        if (funcSize >= initialFuncListSize) {
+            initialFuncListSize *= 2;
+            func_t* newFuncList = (func_t*)realloc(funcList, initialFuncListSize * sizeof(func_t));
+            if (newFuncList == NULL) {
+                free(funcList);
+                // Handle memory allocation error.
+                return;
+            }
+            funcList = newFuncList;
+        }
+
+        funcList[funcSize] = parseFunc(fileInc, tokenInc);
+        if (bError) {
+            free(funcList);  // Free the memory allocated.
+            return;
+        }
+        funcSize++;
+    }
 }
