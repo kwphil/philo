7abeaf3f0046a9fa27d423f42cd9da454a856598
kwphil <121273949+kwphil@users.noreply.github.com>
2023-11-22 13:11:56 -0600

    Update cmplToolchain.c

--- asm/cmpl/cmplToolchain.c
+++ asm/cmpl/cmplToolchain.c
@@ -1,3 +1,14 @@
 #include "cmplToolchain.h"
+#include <stdint.h>
 
-cmplToolchain_t cmpl = setCmplToolchain;
+cmplToolchain_t cmpl = setCmplToolchain();
+
+setCmplToolchain() {
+    cmplToolchain_t ret;
+
+    ret.setCompileUntil = cmplToolchain_SetCompileUntil;
+}
+
+cmplToolchain_SetCompileUntil(char *input, uint64_t data) {
+    
+}
