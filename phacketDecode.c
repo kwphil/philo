26241806e2b2a16b3d25a90be6de4c96fb4026b1
kwphil <121273949+kwphil@users.noreply.github.com>
2023-12-06 10:56:37 -0600

    Update phacketDecode.c

--- phacketDecode.c
+++ phacketDecode.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include "srcc/file/file.h"
 #include "srcc/string/string.h"
+
+char *prgmName;
+char *currWord = (char *)malloc(sizeof(char));
+uint32_t index = 0;
 
 int main(int argc, char *argv[]) {
     int line_count = atoi(argv[1]);
     int file_index = 0;
     char *file = readFile("temp.txt");
+
+    prgmName = malloc(sizeof(argv[2]));
+    strcpy(prgmName, argv[2]);
 
     if(file == NULL) {
         printf("Error! could not read the file!");
@@ -28,10 +36,7 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void decodeLine(char *currLine) {
-    int index = 0;
-    char *currWord = nextWord(currLine, index);
-
+void decodeLine(char *currLine, int line, int c) {
     //We want to inc the index in order to account for the whitespace between 
     switch(1) {
         case(strcmp(currWord, "new")):
@@ -66,27 +71,33 @@ void decodeLine(char *currLine) {
             if(strcmp(currWord, "replace")) {
                 currWord = nextWord(currWord, ++index);
                 if(strcmp(currWord, "ALL")) {
-                    if(strcmp(currWord, "FILE")) {
-                        system(appendf("touch temp.txt && wget https://raw.githubusercontent.com/kwphil/phacket/%s -O temp.txt", nextWord(currWord, ++index)));
-                        system(appendf("cat %s >> %s", "temp.txt", filename));
-                        system("rm temp.txt");
-
-                        return;
-                    }
-                if(strcmp(currWord, "TEXT")) {
-                    char *insert = (char *)malloc(sizeof(char));
-                    size_t size = 1;
-                    while(currLine[index++] != '\n') {
-                        realloc(insert, ++size);
-                        currLine[index] = insert[size - 1];
-                    }
-                    system(appendf("%s >> %s", insert, filename));
-                    
-                    return;
-                }
+                    getString();
+                    system("temp.txt >> %s", filename)
                 }
             }
-            return;
+    }
+}
+
+void getString() {
+    if(strcmp(currWord, "FILE")) {
+        system(appendf("touch temp.txt && wget https://raw.githubusercontent.com/kwphil/phacket/%s/%s -O temp.txt", prgmName, nextWord(currWord, ++index)));
+
+        return;
+    }
+    if(strcmp(currWord, "TEXT")) {
+        char *insert = (char *)malloc(sizeof(char));
+        size_t size = 1;
+        while(currLine[index++] != '\n') {
+            realloc(insert, ++size);
+            currLine[index] = insert[size - 1];
+        }
+        system(appendf("touch temp.txt && %s >> temp.txt", insert));
+
+        return;
+    }
+    else {
+        printf("Error at %d:%d. Data type: %s not recognized", line, c, currWord);
+        exit(-1);
     }
 }
 
