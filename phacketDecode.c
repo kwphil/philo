77495b483c2ff38033166c6d6802ddad6738cdb5
kwphil <121273949+kwphil@users.noreply.github.com>
2023-12-07 10:09:32 -0600

    Debug phacketDecode.c

--- phacketDecode.c
+++ phacketDecode.c
@@ -9,11 +9,13 @@ char *currWord = (char *)malloc(sizeof(char));
 uint32_t index = 0;
 
 int main(int argc, char *argv[]) {
+    char *prgmName = (char *)malloc(sizeof(argv[2])), *currWord = (char *)malloc(sizeof(char));
+    uint32_t index = 0;
+    
     int line_count = atoi(argv[1]);
     int file_index = 0;
     char *file = readFile("temp.txt");
 
-    prgmName = malloc(sizeof(argv[2]));
     strcpy(prgmName, argv[2]);
 
     if(file == NULL) {
@@ -21,16 +23,16 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    for(int i = 0; i < line_count; i++) {
+    for(int i = 0, j = 0; i < line_count; i++) {
         size_t size = 1;
         char *currLine = (char *)malloc(sizeof(char));
 
-        for (int j = 0; j < line_count; j++) {
+        for (; currLine[j] != '\n'; j++) {
             realloc(currLine, ++size);
             currLine[size - 1] = file[file_index++];
         }
 
-        decodeLine(currLine);
+        decodeLine(currLine, i, j, prgmName, currWord, index);
     }
 
     free(prgmName);
@@ -38,44 +40,44 @@ int main(int argc, char *argv[]) {
     return 0;
 }
 
-void decodeLine(char *currLine, int line, int c) {
+void decodeLine(char *currLine, int line, int c, char *prgmName, char *currWord, int index) {
     //We want to inc the index in order to account for the whitespace between 
-    switch(1) {
-        case(strcmp(currWord, "new")):
-            currWord = nextWord(currLine, ++index);
-            if(strcmp(currWord), "DIR") system(appendStr("mkdir ", nextWord(currLine, ++index)));
-            else if(strcmp(currWord), "FILE") system(appendStr("touch ", nextWord(currLine, ++index)));
+    if(strcmp(currWord, "new")) {
+        currWord = nextWord(currLine, currWord, ++index);
+        if(strcmp(currWord, "DIR")) system(appendStr("mkdir ", nextWord(currLine, currWord, index)));
+        else if(strcmp(currWord, "FILE")) system(appendStr("touch ", nextWord(currLine, currWord, index)));
         return;
-        case(strcmp(currWord, "edit")):
-            char *filename = nextWord(currLine, ++index);
-            currWord = nextWord(currWord, ++index);
-            if(strcmp(currWord, "append")) {
-                getString();
-                system(appendf("temp.txt >> %s && rm temp.txt", filename));
+    }
+    if(strcmp(currWord, "edit")) {
+        char *filename = nextWord(currLine, currWord, index);
+        currWord = nextWord(currLine, currWord, index);
+        if(strcmp(currWord, "append")) {
+            nextWord(currLine, currWord, index);
+            system(appendf("temp.txt >> %s && rm temp.txt", filename));
+        }
+        else if(strcmp(currWord, "replace")) {
+            currWord = nextWord(currLine, currWord, index);
+            if(strcmp(currWord, "ALL")) {
+                nextWord(currLine, currWord, index);
+                system(appendf("cp -f temp.txt %s && rm temp.txt", filename));
+            } else if(isNum(currWord)) {
+                nextWord(currLine, currWord, index);
+                system("$TEXT=$(cat temp.txt)");
+                system(appendf("sed '%d s/.*/$TEXT' %s && rm temp.txt", atoi(currWord), filename));
+            } else {
+                printf("Error at %d:%d. Data type: %s not recognized", line, c, currWord);
+                exit(-2);
             }
-            else if(strcmp(currWord, "replace")) {
-                currWord = nextWord(currWord, ++index);
-                if(strcmp(currWord, "ALL")) {
-                    getString();
-                    system(appendf("cp -f temp.txt %s && rm temp.txt", filename));
-                } else if(isNum(currWord)) {
-                    getString();
-                    system("$TEXT=$(cat temp.txt)");
-                    system(appendf("sed '%d s/.*/$TEXT' %s && rm temp.txt", atoi(currWord), filename));
-                } else {
-                    printf("Error at %d:%d. Data type: %s not recognized", line, c, currWord);
-                    exit(-2);
-                }
-            }
-            free(filename);
+        }
+        free(filename);
         return;
     }
 }
 
-void getString() {
-    currWord = nextWord(currLine);
+void getString(char *currWord, char *currLine, char *prgmName, uint32_t index) {
+    currWord = nextWord(currLine, currWord, ++index);
     if(strcmp(currWord, "FILE")) {
-        system(appendf("touch temp.txt && wget https://raw.githubusercontent.com/kwphil/phacket/main/%s/%s -O temp.txt", prgmName, nextWord(currWord, ++index)));
+        system(appendf("touch temp.txt && wget https://raw.githubusercontent.com/kwphil/phacket/main/%s/%s -O temp.txt", prgmName, nextWord(currLine, currWord, index)));
 
         return;
     }
@@ -92,12 +94,12 @@ void getString() {
         return;
     }
     else {
-        printf("Error at %d:%d. Data type: %s not recognized", line, c, currWord);
+        printf("Error in string %s Data type: %s not recognized", currLine, currWord);
         exit(-1);
     }
 }
 
-char *nextWord(char *currLine) {
+char *nextWord(char *currLine, char *currWord, uint32_t index) {
     currWord = realloc(currWord, sizeof(char));
 
     for(int i = 0; i != ' '; i++) {
