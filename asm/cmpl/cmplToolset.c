ceacec5bdcf707777fa9b61d210c6e0bf180f950
kwphil <121273949+kwphil@users.noreply.github.com>
2023-12-12 09:59:55 -0600

    Update cmplToolset.c

--- asm/cmpl/cmplToolset.c
+++ asm/cmpl/cmplToolset.c
@@ -3,14 +3,30 @@
 
 cmplToolchain_t cmpl = setCmplToolchain();
 
+int cmpl_stop_i;
+void *cmpl_stop_data;
+int instruct_num = 0;
+
 void setCmplToolchain() {
     cmpl.setCompileUntil = cmplToolchain_SetCompileUntil;
 }
 
-cmplToolchain_SetCompileUntil(char *input, char *data) {
+cmplToolchain_SetCompileUntil(char *input, void *data) {
     cmpl.stopCmpl = false;
 
-    if(strcmp(input, "instruct-num")) {
+    if(strcmp(input, "instruct-num"))
+        cmpl_stop_i = 0;
+
+    cmpl_stop_data = data;
+}
+
+cmplToolcahin_CheckStop() {
+    switch(cmpl_stop_i) {
+        case(0):
+            if((int)cmpl_stop_data <= instruct_num)
+                cmpl.stopCmpl = true;
+        return;
+
         
     }
 }
