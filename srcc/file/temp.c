5a921f81617942a5d497fd8c16b82ac9a11f6f5f
kwphil <121273949+kwphil@users.noreply.github.com>
2023-12-12 19:52:36 -0600

    Update temp.x

--- srcc/file/temp.c
+++ srcc/file/temp.c
@@ -1,16 +1,32 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <unistd.h>
+#include <sys/types.h>
 
-void tempWrite(char *filename, void *write, uint32_t size) {
+void tempPush(char *filename, void *write, size_t size) {
 
-    FILE *fptr = fopen(filename "w");
+    FILE *fptr = fopen(filename, "w");
     if(fptr == NULL) {
         printf("Compiler Error! Temp file %s not found", filename);
         exit(14);
     }
 
-    fprintf(filename, appendStr((char [size])write, '\n')); //make sure we add a null-pointer to convert it to a string
+    fprintf(filename, appendStr((char [size])write, '\0')); //make sure we add a null-pointer to convert it to a string
 
     fclose(fptr);
+}
+
+void *tempPop(char *filename, size_t size) {
+    uint64_t i = 0;
+    FILE *fptr = fopen(filename, "r");
+
+    while(i++ < size) {
+        fseek(fptr, 0, )
+    }
+
+    fseek(f, 0, SEEK_END);
+    int fSize = ftell(f) - size;
+
+    truncate(filename, fSize);
 }
--- srcc/file/temp.h
+++ srcc/file/temp.h
@@ -1,9 +1,10 @@
 #ifndef CFTEMP_H
 #define CFTEMP_H
 
+#include <stdlib.h>
+
 //Used to define temperary files, which will be used to help relieve usage on ram
 //Note that this can be dangerous, do not use unless necessary, and you know what you are doing.
 
-//Writes a temperary file
-//If programs need to take up more data than 0xFFFFFFFF chars at a time (for some reason), we can just change the size value to uint64_t
-void tempWrite(char *filename, void *write, uint32_t size);
+//Appends to a specified file
+void tempWrite(char *filename, void *write, size_t size);
