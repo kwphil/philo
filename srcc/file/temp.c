1108c3ced78d3bb273fcf926eae306d34a9f9b19
kwphil <121273949+kwphil@users.noreply.github.com>
2023-12-12 19:32:23 -0600

    Update temp.c

--- srcc/file/temp.c
+++ srcc/file/temp.c
@@ -3,8 +3,6 @@
 #include <stdbool.h>
 
 void tempWrite(char *filename, void *write, uint32_t size) {
-    register uint32_t i = 0;
-    uint8_t inc = 255;
 
     FILE *fptr = fopen(filename "w");
     if(fptr == NULL) {
@@ -12,9 +10,7 @@ void tempWrite(char *filename, void *write, uint32_t size) {
         exit(14);
     }
 
-    while(i <= size)
-        if(i + inc > size) inc >>= 1; //Diving by 2
-        fprintf(filename, appendStr((char [inc])write[i += inc], '\n')); //make sure we add a null-pointer to convert it to a string
+    fprintf(filename, appendStr((char [size])write[i += inc], '\n')); //make sure we add a null-pointer to convert it to a string
 
     fclose(fptr);
 }
