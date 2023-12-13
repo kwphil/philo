550b3a82c1b387f1c63d715ed26afd234c1f898f
kwphil <121273949+kwphil@users.noreply.github.com>
2023-12-13 09:11:08 -0600

    Update build.c

--- asm/build/build.c
+++ asm/build/build.c
@@ -1,3 +1,4 @@
+#include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include "../../srcc/file/file.h"
@@ -6,18 +7,15 @@
 #include "../../srcp/types/types.h"
 
 void build(const asm_t *asmList, const char *argv) {
-    if(atoi(argv[2]) != 1 || argv[3] == NULL) {
-        argv[3] = (char *)malloc((size_t)6);
-        strcpy(argv[3], "asm.s");
-    } else system(appendStr("touch ", argv[3]));
+    FILE *fptr = fopen("asm.s", "a");
 
-    system(appendStr("\"section .text\nglobal _start\n_start: \" >> ", argv[3])); //start the program here
+    fprintf(appendStr("section .text\nglobal _start\n_start: \n")); //start the program here
     int i = 0, loc = 0, sect = 0;
 
     while (1) {
         while (asmList[i].ins[0] != '\0') {
             if(asmList[i].section == sect && asmList[i].loc == loc) {
-                system(appendf("\"%s \" >> %s", argv[3], asmList[i].ins));
+                fprintf(asmList[i].ins);
                 i = 0;
                 ++loc;
 
@@ -27,18 +25,18 @@ void build(const asm_t *asmList, const char *argv) {
 
         if (sect <= 3) {
             loc = 0;
-            system(appendStr("\"section .\" >> ", argv[3]));
-            if (sect == 1) system(appendStr("\"data \" >> ", argv[3]));
-            else system(appendStr("\"bss \" >> ", argv[3]));
+            fprintf("section .");
+            if (sect == 1) fprintf("data ");
+            else fprintf("bss ");
             sect++;
         } else break;
     }
 
+    fclose(fptr);
+
     // Execute the assembler with the correct path
-    if(atoi(argv[2]) == 0) {
-        system(appendf("nasm %s -o build.bin && rm %s", argv[3], argv[3]));
-        exit(0);
-    }
+    system(appendf("nasm asm.s -o %s && rm asm.s", (argv[3] == NULL || argv[2] == 0) ? "test.bin" : argv[3]));
+    if(argv[2] == 0) system("./test.bin && rm test.bin");
 
     exit(0);
 }
