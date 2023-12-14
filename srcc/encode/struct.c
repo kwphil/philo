7037104a1a57f1de5ded9221de43712aeb277e16
kwphil <121273949+kwphil@users.noreply.github.com>
2023-12-14 10:44:15 -0600

    Update struct.c

---
+++ srcc/encode/struct.c
@@ -0,0 +1,10 @@
+#include <stdint.h>
+#include <stdlib.h>
+
+uint8_t structEncode(const void *strct, register size_t size) {
+    uint8_t *ret = (uint8_t *)malloc(sizeof(uint8_t) * size);
+
+    for(register uint64_t i = 0; i < size; ++i) {
+        ret[i] = *(uint8_t)(&strct + size);
+    }
+} 
